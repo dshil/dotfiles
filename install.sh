bae516b7ec33670ae1cc7518979ea98288e15198
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-11-12 14:22:40 +0300

    vim: remove custom rtags installation

--- install.sh
+++ install.sh
@@ -4,11 +4,3 @@ ln -s -f $(pwd)/.irbrc ~/.irbrc
 ln -s -f $(pwd)/.gitconfig ~/.gitconfig
 
 rm $(pwd)/.vim/.vim 2>/dev/null
-
-# Don't forget to exec :Helptags
-vim -u NONE -c "helptags vim-fugitive/doc" -c q
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install rtags
-    brew services restart rtags
-fi
