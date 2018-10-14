5386ad79c8a3376f6afed1726c5d05c0587115c2
Dmitriy Shilin <sdadeveloper@gmail.com>
2018-10-14 08:35:17 +0300

    scripts: prepare setup for vanilla fedora

---
+++ fedoraessential.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -xe
+
+function setup_chrome () {
+    dnf install fedora-workstation-repositories
+    dnf config-manager --set-enabled google-chrome
+    dnf install google-chrome-stable
+}
+
+function setup_terminal () {
+    dnf install rxvt-unicode-256color
+    dnf install terminus-fonts
+}
+
+function setup_toolchain () {
+    dnf install git gitk make cmake autoconf gcc-c++
+}
+
+function setup_i3 () {
+    dnf install i3
+    dnf install i3lock
+}
+
+setup_chrome
+setup_terminal
+setup_toolchain
+setup_i3
