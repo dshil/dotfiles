c983b63c1466bc3b040783056149afbaf9e8dfd4
Dmitriy Shilin <dshil@fastmail.com>
2018-12-07 06:59:55 +0300

    scripts: properly install ag

--- scripts/fedoraessential.sh
+++ scripts/fedoraessential.sh
@@ -1,35 +1,44 @@
-#! /bin/bash
+#!/bin/bash
 
 set -xe
 
-function setup_chrome () {
+function setup_chrome ()
+{
     dnf install -y fedora-workstation-repositories
     dnf config-manager --set-enabled google-chrome
     dnf install -y google-chrome-stable
 }
 
-function setup_terminal () {
+function setup_terminal ()
+{
     dnf install -y rxvt-unicode-256color terminus-fonts
 }
 
-function setup_toolchain () {
-    dnf install -y git gitk make cmake autoconf gcc-c++ vim-X11 python3-pip ag
+function setup_toolchain ()
+{
+    dnf install -y git gitk \
+                   make cmake autoconf gcc-c++ \
+                   vim-X11 python3-pip the_silver_searcher
 }
 
-function setup_i3 () {
+function setup_i3 ()
+{
     dnf install -y lightdm i3 i3lock
 }
 
-function setup_utils () {
+function setup_utils ()
+{
     dnf install -y sxiv mupdf ack ctags weechat abook graphviz
 }
 
-function setup_remmina() {
+function setup_remmina()
+{
     dnf copr enable -y hubbitus/remmina-next
     dnf install -y remmina
 }
 
-function setup_samba() {
+function setup_samba()
+{
     dnf install gvfs gvfs-smb sshfs
 }
 
@@ -39,3 +48,4 @@ setup_toolchain
 setup_i3
 setup_utils
 setup_remmina
+setup_samba
