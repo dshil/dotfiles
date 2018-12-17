656c4d858f524bc361d0cfad414032adc6df88b3
Dmitriy Shilin <dshil@fastmail.com>
2018-12-17 08:21:56 +0300

    scripts: add llvm script

---
+++ scripts/linux-gnu/llvm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -xe
+
+releaseno=RELEASE_700
+
+pushd $HOME
+mkdir -p src/compilers
+pushd src/compilers
+svn co http://llvm.org/svn/llvm-project/llvm/tags/$releaseno/final llvm_$releaseno
+pushd llvm_$releaseno/tools
+svn co http://llvm.org/svn/llvm-project/cfe/tags/$releaseno/final clang
+popd # llvm_$releaseno/tools
+popd # src/compilers
+popd # $HOME
+
+mkdir -p /tmp/build/compilers
+pushd /tmp/build/compilers
+mkdir llvm_$releaseno
+pushd llvm_$releaseno
+cmake -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=/usr/bin/gcc \
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+    -DCMAKE_ASM_COMPILER=/usr/bin/gcc \
+    $HOME/src/compilers/llvm_$releaseno
+make -j8
+sudo make -j8 install
+rm -rf /tmp/build/compilers/llvm_$releaseno
+rm -rf $HOME/src/compilers/llvm_$releaseno
+popd # llvm_$releaseno
+popd # /opt/build/compilers
+
+# Reference:
+#  * https://www.vultr.com/docs/how-to-install-llvm-and-clang-on-centos-6
+#  * https://sarcasm.github.io/notes/dev/compilation-database.html
