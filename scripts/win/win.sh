9afb358366e2f177e72078f89a6e3ba673799173
Dmitriy Shilin <dshil@fastmail.com>
2018-12-29 21:52:12 -0800

    scripts/win: remove spaces from packages string

--- scripts/win/win.sh
+++ scripts/win/win.sh
@@ -2,9 +2,6 @@
 
 set -xe
 
-packages="autoconf,automake,bison,cabextract,doxygen,flex,gcc-g++,            \
-gettext-devel,git,gnupg,gperf,make,mintty,nasm,openssh,openssl,patch,perl,    \
-python,python3,pkg-config,rsync,unzip,vim,wget,zip,perl-Archive-Zip,          \
-perl-Font-TTF,perl-IO-String,fzf,vim-X11,the_silver_searcher,gitk,cmake"
+packages="autoconf,automake,bison,cabextract,doxygen,flex,gcc-g++,gettext-devel,git,gnupg,gperf,make,mintty,nasm,openssh,openssl,patch,perl,python,python3,pkg-config,rsync,unzip,vim,wget,zip,perl-Archive-Zip,perl-Font-TTF,perl-IO-String,fzf,vim-X11,the_silver_searcher,gitk,cmake,subversion"
 
 setup-x86_64.exe -q --packages $packages
