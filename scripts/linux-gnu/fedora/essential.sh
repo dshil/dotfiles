8e138c5b4f8c8ab93f8b75c1fb31a81fc25a2f09
Dmitriy Shilin <dshil@fastmail.com>
2019-01-02 08:30:45 +0300

    mail: use antiword for msword docs

--- scripts/linux-gnu/fedora/essential.sh
+++ scripts/linux-gnu/fedora/essential.sh
@@ -18,7 +18,8 @@ function setup_toolchain ()
 {
     dnf install -y git gitk \
                    make cmake autoconf gcc-c++ \
-                   vim-X11 python3-pip the_silver_searcher
+                   vim-X11 python3-pip the_silver_searcher \
+                   antiword
 }
 
 function setup_i3 ()
