3feebffce77f252566dbf3632cc19c772afe6d88
Dmitriy Shilin <dshil@fastmail.com>
2019-01-04 16:21:30 +0300

    systemd: add installer

---
+++ systemd/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -xe
+
+dir=~/.config/systemd/user
+mkdir -p $dir
+
+services=(rdm.service mbsync.service msmtpctl.service)
+timers=(mbsync.timer msmtpctl.timer)
+sockets=(rdm.socket)
+
+known_units=("${services[@]}" "${timers[@]}" "${sockets[@]}")
+active_units=("${timers[@]}" "${sockets[@]}")
+
+for unit in "${known_units[@]}"
+do
+    if [ -f "$dir/$unit" ]; then
+        systemctl --user stop $unit
+        systemctl --user disable $unit
+    fi
+
+    ln -s -f $(pwd)/systemd/$unit $dir/$unit
+done
+
+systemctl --user daemon-reload
+
+for unit in "${active_units[@]}"
+do
+    systemctl --user enable $unit
+    systemctl --user start $unit
+done
