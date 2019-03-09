01548b3fde3682c2ae90534d24c90e10c59dbba0
Dmitriy Shilin <dshil@fastmail.com>
2019-03-09 07:27:07 +0300

    systemd: increase PWM frequency

--- systemd/sys.sh
+++ systemd/sys.sh
@@ -3,13 +3,14 @@
 set -xe
 
 sysdir=/usr/lib/systemd/system
+freq=0x1E801E8
 
-systemctl --system stop    pwmfrequency@0x7a107a1
-systemctl --system disable pwmfrequency@0x7a107a1
+systemctl --system stop    pwmfrequency@$freq
+systemctl --system disable pwmfrequency@$freq
 
 sudo rm -rf $sysdir/pwmfrequency@.service
 sudo cp $(pwd)/systemd/pwmfrequency@.service $sysdir
 
 systemctl --system daemon-reload
-systemctl --system enable pwmfrequency@0x7a107a1
-systemctl --system start  pwmfrequency@0x7a107a1
+systemctl --system enable pwmfrequency@$freq
+systemctl --system start  pwmfrequency@$freq
