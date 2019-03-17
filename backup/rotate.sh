d0d4281c3d73ea614e38d8be5d7932c9c71bf3dd
Dmitriy Shilin <dshil@fastmail.com>
2019-03-17 17:02:22 +0300

    backup: rework rotate script

--- backup/rotate.sh
+++ backup/rotate.sh
@@ -4,14 +4,13 @@
 #
 # MAXENTRYNUM determines the maximum number of backups in the provided $SRC
 # backup directory.
-
 set -xe
 
 if [ "$SRC" == "" ]; then
     echo "error: backup source path must be specified"; exit 1
 fi
 
-ls -tp $SRC \
-    | grep -v '/$' \
-    | tail -n +$(expr $MAXENTRYNUM + 1) \
-    | xargs --no-run-if-empty rm
+find $SRC -maxdepth 1 -type d \
+    | sort -M \
+    | tail -n $MAXENTRYNUM \
+    | xargs --no-run-if-empty rm -rf
