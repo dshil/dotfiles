68997a80f90313c74058c16538c5aab1ee2ae57b
Dmitriy Shilin <dshil@fastmail.com>
2019-04-03 12:57:29 +0400

    scripts: rework fedora toolchain

--- scripts/linux-gnu/fedora/essential.sh
+++ scripts/linux-gnu/fedora/essential.sh
@@ -2,58 +2,16 @@
 
 set -xe
 
-function setup_chrome ()
-{
-    dnf install -y fedora-workstation-repositories
-    dnf config-manager --set-enabled google-chrome
-    dnf install -y google-chrome-stable
-}
+sudo dnf config-manager --set-enabled google-chrome
 
-function setup_terminal ()
-{
-    dnf install -y rxvt-unicode-256color terminus-fonts
-}
+sudo dnf install -y fedora-workstation-repositories \
+    google-chrome-stable \
+    rxvt-unicode-256color terminus-fonts \
+    git gitk svn make cmake autoconf gcc-c++ \
+    vim-X11 python3-pip the_silver_searcher htop \
+    antiword okular acpid xss-lock davfs2 \
+    lightdm i3 i3lock light \
+    sxiv mupdf ack ctags weechat abook graphviz \
+    gvfs gvfs-smb sshfs curl cabextract xorg-x11-font-utils fontconfig
 
-function setup_toolchain ()
-{
-    dnf install -y git gitk \
-                   make cmake autoconf gcc-c++ \
-                   vim-X11 python3-pip the_silver_searcher \
-                   antiword okular acpid xss-lock davfs2
-}
-
-function setup_i3 ()
-{
-    dnf install -y lightdm i3 i3lock light
-}
-
-function setup_utils ()
-{
-    dnf install -y sxiv mupdf ack ctags weechat abook graphviz
-}
-
-function setup_remmina()
-{
-    dnf copr enable -y hubbitus/remmina-next
-    dnf install -y remmina
-}
-
-function setup_samba()
-{
-    dnf install gvfs gvfs-smb sshfs
-}
-
-function setup_fonts()
-{
-    dnf install curl cabextract xorg-x11-font-utils fontconfig
-    rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-    fc-cache -fv
-}
-
-setup_chrome
-setup_terminal
-setup_toolchain
-setup_i3
-setup_utils
-setup_remmina
-setup_samba
+fc-cache -fv
