36a4f6cc465c3aadd973ee59246693f3a857dcb2
Dmitriy Shilin <dshil@fastmail.com>
2019-05-23 15:05:55 +0200

    scripts: ensure that swap directory is created

--- scripts/linux-gnu/swap.sh
+++ scripts/linux-gnu/swap.sh
@@ -3,8 +3,13 @@
 set -xe
 
 swapfile=/swap/file
+swapdir=/swap
+
+mkdir -p $swapdir
 
 if [ ! -f $swapfile ]; then
+    touch $swapfile
+
     dd if=/dev/zero of=$swapfile bs=1GB count=14
     chmod 0600 $swapfile
 fi
