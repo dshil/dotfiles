be0c4fae1ac65cdae653b5b032d7a96e4d69ab9a
Dmitriy Shilin <dshil@fastmail.com>
2019-08-27 10:00:09 +0300

    scripts: add perl-mail-box for notmuch search

--- scripts/linux-gnu/arch/essential.sh
+++ scripts/linux-gnu/arch/essential.sh
@@ -7,4 +7,5 @@ sudo pacman -Sy \
        python3 git tk svn make cmake autoconf the_silver_searcher htop \
        okular acpid light ack ctags weechat abook gvfs gvfs-smb sshfs curl \
        fontconfig isync msmtp ripmime w3m urlscan gnupg neomutt notmuch perl \
-       perl-string-shellquote perl-mail-box perl-mailtools keepassxc gvim
+       perl-string-shellquote perl-mail-box perl-mailtools perl-mail-box \
+       keepassxc gvim
