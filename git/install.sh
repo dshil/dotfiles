3519a7570c6c8dea6826b6c2d58d76f8489420c8
Dmitriy Shilin <dshil@fastmail.com>
2019-09-08 05:18:20 +0300

    git: handle hooks

--- git/install.sh
+++ git/install.sh
@@ -5,9 +5,14 @@ set -xe
 dir=$(pwd)
 
 ln -sf $dir/git/config ~/.gitconfig
+ln -snf $dir/git/template ~/.git_template
+
+git config --global init.templatedir '~/.git_template'
 
 if [[ "$OSTYPE" == "cygwin" ]]; then
     git config --global core.preloadindex true
     git config --global core.fscache true
     git config --global gc.auto 256
 fi
+
+# vim:syntax=sh
